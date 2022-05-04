6277cc1b829b8ce789748167fe50eb4470b04660
teklinco <teklinco>
2022-05-04 12:03:47 +0800

    added port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 
 //home route
@@ -12,6 +14,8 @@ app.get("/", (req, res) => {
 });
 
 //start the server
-app.listen(3000, () => {
-  console.log(`Server is up and running on port 3000`);
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
 });
+
+console.log(process.env.PORT);
